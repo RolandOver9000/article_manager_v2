f4ffdfcd9a8bcf75423bcdf8a7f7d0dbbbfca16a
Roland Csosz <roland@iot-inspector.com>
2021-08-17 13:30:08 +0200

    Initialized the project

---
+++ src/test/java/com/codecool/articlemanager/articlemanager/ArticlemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.articlemanager.articlemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
