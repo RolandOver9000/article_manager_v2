404d3d0f6f490d83f6b43bfed8644be5477b9110
Roland Csosz <roland@iot-inspector.com>
2021-08-18 11:34:16 +0200

    Added endpoint for getting all articles

--- src/main/java/com/codecool/articlemanager/articlemanager/controller/ArticleController.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/controller/ArticleController.java
@@ -1,9 +1,14 @@
 package com.codecool.articlemanager.articlemanager.controller;
 
+import com.codecool.articlemanager.articlemanager.model.ArticleEntity;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -11,5 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class ArticleController {
 
+    private final ArticleService articleService;
 
+    @GetMapping("")
+    public ResponseEntity<List<ArticleEntity>> getAllArticles() {
+        return ResponseEntity.ok(articleService.getAllArticles());
+    }
 }
