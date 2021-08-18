85c6682e42845d6d5000407f96bb5ce47acc8486
Roland Csosz <roland@iot-inspector.com>
2021-08-18 11:53:09 +0200

    Implemented repository for getting articles

---
+++ src/main/java/com/codecool/articlemanager/articlemanager/repository/ArticleRepository.java
@@ -0,0 +1,12 @@
+package com.codecool.articlemanager.articlemanager.repository;
+
+import com.codecool.articlemanager.articlemanager.model.ArticleEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends CrudRepository<ArticleEntity, Long> {
+    List<ArticleEntity> findAll();
+}
--- src/main/java/com/codecool/articlemanager/articlemanager/service/ArticleService.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/service/ArticleService.java
@@ -1,16 +1,19 @@
 package com.codecool.articlemanager.articlemanager.service;
 
 import com.codecool.articlemanager.articlemanager.model.ArticleEntity;
+import com.codecool.articlemanager.articlemanager.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
     public List<ArticleEntity> getAllArticles() {
-        return articleRepository.getAllArticles();
+        return articleRepository.findAll();
     }
 }
