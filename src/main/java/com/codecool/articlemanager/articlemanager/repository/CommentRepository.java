3d4e6d125e6158cba2b560d056880c78a5fe4c01
Roland Csosz <roland@iot-inspector.com>
2021-08-19 10:39:50 +0200

    Created a service function to get comments by article id

--- src/main/java/com/codecool/articlemanager/articlemanager/repository/CommentRepository.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/repository/CommentRepository.java
@@ -4,7 +4,11 @@ import com.codecool.articlemanager.articlemanager.model.CommentEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends CrudRepository<CommentEntity, Long> {
+
+    List<CommentEntity> findAllByArticleId(Long id);
 
 }
--- src/main/java/com/codecool/articlemanager/articlemanager/service/CommentService.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/service/CommentService.java
@@ -1,11 +1,18 @@
 package com.codecool.articlemanager.articlemanager.service;
 
+import com.codecool.articlemanager.articlemanager.model.CommentEntity;
 import com.codecool.articlemanager.articlemanager.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+
+    public List<CommentEntity> getCommentsByArticleId(Long id) {
+        return commentRepository.findAllByArticleId(id);
+    }
 }
