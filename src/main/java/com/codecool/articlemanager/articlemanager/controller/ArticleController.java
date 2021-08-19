5a46108362b845d7c85899fd937f2e47501cb7fb
Roland Csosz <roland@iot-inspector.com>
2021-08-19 15:02:56 +0200

    Separated comment related endpoints

--- src/main/java/com/codecool/articlemanager/articlemanager/controller/ArticleController.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/controller/ArticleController.java
@@ -1,10 +1,7 @@
 package com.codecool.articlemanager.articlemanager.controller;
 
-import com.codecool.articlemanager.articlemanager.model.dto.IncomingComment;
 import com.codecool.articlemanager.articlemanager.model.entity.ArticleEntity;
-import com.codecool.articlemanager.articlemanager.model.entity.CommentEntity;
 import com.codecool.articlemanager.articlemanager.service.ArticleService;
-import com.codecool.articlemanager.articlemanager.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +15,6 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
-    private final CommentService commentService;
 
     @GetMapping("")
     public ResponseEntity<List<ArticleEntity>> getAllArticles() {
@@ -47,36 +43,5 @@ public class ArticleController {
             return ResponseEntity.ok("Successfully deleted the article.");
         }
         return ResponseEntity.ok("There were a problem during the deletion process.");
-    }
-
-    //Endpoints for article comments
-
-    @GetMapping("/{id}/comments")
-    public ResponseEntity<List<CommentEntity>> getAllCommentsByArticleId(@PathVariable(value="id") Long id) {
-        return ResponseEntity.ok(commentService.getCommentsByArticleId(id));
-    }
-
-    @PostMapping("/{id}/comments")
-    public ResponseEntity<String> saveCommentForArticle(@PathVariable(value="id") Long articleId,
-                                                        @RequestBody IncomingComment comment) {
-        try{
-            commentService.saveCommentByArticleId(articleId, comment);
-            return ResponseEntity.ok("Comment successfully saved.");
-        } catch (Exception e) {
-            System.out.println("Error during saving a comment.");
-        }
-            return ResponseEntity.ok("There were a problem during saving your comment.");
-    }
-
-    @DeleteMapping("/{articleId}/comments/{commentId}")
-    public ResponseEntity<String> deleteCommentById(@PathVariable(value="articleId") Long articleId,
-                                                    @PathVariable(value="commentId") Long commentId) {
-        try{
-            commentService.deleteCommentById(commentId);
-            return ResponseEntity.ok("Comment successfully delete.");
-        } catch (Exception e) {
-            System.out.println("Error during the comment deletion process.");
-        }
-        return ResponseEntity.ok("There was a problem during deleting your comment.");
     }
 }
