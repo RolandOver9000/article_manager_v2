bb22d29da55e6dfeca87929fc2c8961e2d7956a7
Roland Csosz <roland@iot-inspector.com>
2021-08-23 13:36:14 +0200

    Created a basic service class for handling JWT tokens and added create jwt token function

---
+++ src/main/java/com/codecool/articlemanager/articlemanager/security/service/JwtService.java
@@ -0,0 +1,52 @@
+package com.codecool.articlemanager.articlemanager.security.service;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class JwtService {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliSeconds;
+
+    private final String rolesFieldName = "roles";
+
+    private final String userIdFieldName = "id";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, List<String> roles, String id) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(rolesFieldName, roles);
+        claims.put(userIdFieldName, id);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliSeconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
