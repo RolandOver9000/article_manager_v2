0befcb369a3141e2f9f2c4e3c534be4dadd7b2aa
Roland Csosz <roland@iot-inspector.com>
2021-08-23 15:30:06 +0200

    Created repository for users

---
+++ src/main/java/com/codecool/articlemanager/articlemanager/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.codecool.articlemanager.articlemanager.repository;
+
+import com.codecool.articlemanager.articlemanager.model.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+}
