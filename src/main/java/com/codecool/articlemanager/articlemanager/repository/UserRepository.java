397e75cea62375b109785839acfcfa57ab945bc9
Roland Csosz <roland@iot-inspector.com>
2021-08-24 15:43:26 +0200

    Added find user by email method for the repository

--- src/main/java/com/codecool/articlemanager/articlemanager/repository/UserRepository.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.codecool.articlemanager.articlemanager.model.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
+
+    Optional<UserEntity> findByEmail(String email);
 }
