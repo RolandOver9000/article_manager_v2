32c47d2fd458617027f8b9781b1c5366536e02b2
Roland Csosz <roland@iot-inspector.com>
2021-08-24 15:12:21 +0200

    Refactored registration endpoint

--- src/main/java/com/codecool/articlemanager/articlemanager/controller/AuthenticationController.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.codecool.articlemanager.articlemanager.controller;
 import com.codecool.articlemanager.articlemanager.model.dto.LoginDTO;
 import com.codecool.articlemanager.articlemanager.model.dto.RegistrationDTO;
 import com.codecool.articlemanager.articlemanager.service.LoginService;
+import com.codecool.articlemanager.articlemanager.service.RegistrationService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthenticationController {
 
     private final LoginService loginService;
+    private final RegistrationService registrationService;
 
     @PostMapping("/login")
     public Long login(@RequestBody LoginDTO loginDTO, HttpServletResponse response) {
@@ -26,8 +28,14 @@ public class AuthenticationController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<RegistrationResponseDTO> registration(@RequestBody RegistrationDTO registrationDTO) {
-        return ResponseEntity.ok(registrationService.register(registrationDTO));
+    public ResponseEntity<String> registration(@RequestBody RegistrationDTO registrationDTO) {
+        try {
+            registrationService.register(registrationDTO);
+            return ResponseEntity.ok("Registration is successful.");
+        } catch (Exception e) {
+            System.out.println("Error during registration.");
+        }
+        return ResponseEntity.ok("Error during registration.");
     }
 
 }
