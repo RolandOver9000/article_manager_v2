e150dfb395b7fae11cffa1e956c25c969aea0c39
Roland Csosz <roland@iot-inspector.com>
2021-08-24 14:59:45 +0200

    Implemented a login process

--- src/main/java/com/codecool/articlemanager/articlemanager/service/LoginService.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/service/LoginService.java
@@ -1,14 +1,24 @@
 package com.codecool.articlemanager.articlemanager.service;
 
 import com.codecool.articlemanager.articlemanager.model.dto.LoginDTO;
+import com.codecool.articlemanager.articlemanager.model.entity.UserEntity;
+import com.codecool.articlemanager.articlemanager.repository.UserRepository;
+import com.codecool.articlemanager.articlemanager.security.service.JwtService;
+import com.codecool.articlemanager.articlemanager.service.cookie.CookieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -16,6 +26,9 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final CookieService cookieService;
 
     public Long tryLogin(LoginDTO loginDTO, HttpServletResponse response) {
         Authentication authentication = tryAuthenticate(loginDTO);
@@ -37,6 +50,27 @@ public class LoginService {
         );
     }
 
-    
+    private ResponseCookie generateResponseCookie(UserEntity userEntity, List<String> roles) {
+        String token = jwtService.createToken(userEntity.getUsername(), roles, Long.toString(userEntity.getId()));
+        return cookieService.createTokenCookie(token, Duration.ofHours(24));
+    }
+
+    private Long login(Authentication authentication, HttpServletResponse response) {
+        List<String> roles = getRolesFrom(authentication);
+        String username = authentication.getName();
+        //try to collect only the id of the user
+        UserEntity foundUser = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        ResponseCookie generatedCookie = generateResponseCookie(foundUser, roles);
+        response.addHeader("Set-Cookie", generatedCookie.toString());
+        return foundUser.getId();
+    }
+
+    private List<String> getRolesFrom(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+    }
 
 }
