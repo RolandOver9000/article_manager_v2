086345f0343189f6ee10d5c8856a0657798158bc
Roland Csosz <roland@iot-inspector.com>
2021-08-24 15:07:45 +0200

    Created an endpoint for registration

--- src/main/java/com/codecool/articlemanager/articlemanager/controller/AuthenticationController.java
+++ src/main/java/com/codecool/articlemanager/articlemanager/controller/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.codecool.articlemanager.articlemanager.controller;
 
 import com.codecool.articlemanager.articlemanager.model.dto.LoginDTO;
+import com.codecool.articlemanager.articlemanager.model.dto.RegistrationDTO;
 import com.codecool.articlemanager.articlemanager.service.LoginService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +23,11 @@ public class AuthenticationController {
     public Long login(@RequestBody LoginDTO loginDTO, HttpServletResponse response) {
         log.info("Login request received: " + loginDTO.toString());
         return loginService.tryLogin(loginDTO, response);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<RegistrationResponseDTO> registration(@RequestBody RegistrationDTO registrationDTO) {
+        return ResponseEntity.ok(registrationService.register(registrationDTO));
     }
 
 }
