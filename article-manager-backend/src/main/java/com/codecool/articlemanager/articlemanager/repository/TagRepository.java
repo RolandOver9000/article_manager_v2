0445e206ab074c481eebda63afcadf7a253e633e
Roland Csosz <roland@iot-inspector.com>
2021-08-31 13:06:39 +0200

    Created tag repository for tag crud operations

---
+++ article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.articlemanager.articlemanager.repository;
+
+import com.codecool.articlemanager.articlemanager.model.entity.TagEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<TagEntity, Long> {
+}
