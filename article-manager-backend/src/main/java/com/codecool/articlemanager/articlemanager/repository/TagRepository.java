bcd86fac2b829da4e7427dab8fd001bda81ad3bc
Roland Csosz <roland@iot-inspector.com>
2021-08-31 16:18:08 +0200

    Added function to tag repository to find tag entity by tag

--- article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/repository/TagRepository.java
+++ article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/repository/TagRepository.java
@@ -4,6 +4,10 @@ import com.codecool.articlemanager.articlemanager.model.entity.TagEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends CrudRepository<TagEntity, Long> {
+
+    Optional<TagEntity> findByTag(String tag);
 }
