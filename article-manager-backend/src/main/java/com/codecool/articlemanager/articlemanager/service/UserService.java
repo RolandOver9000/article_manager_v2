d3107340ff6137348308e322438c179590f7fd68
Roland Csosz <roland@iot-inspector.com>
2021-09-02 14:07:19 +0200

    Implemented a service function for getting user data

--- article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/service/UserService.java
+++ article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/service/UserService.java
@@ -1,7 +1,9 @@
 package com.codecool.articlemanager.articlemanager.service;
 
+import com.codecool.articlemanager.articlemanager.model.dto.OutgoingUserDTO;
 import com.codecool.articlemanager.articlemanager.model.entity.UserEntity;
 import com.codecool.articlemanager.articlemanager.repository.UserRepository;
+import com.codecool.articlemanager.articlemanager.security.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     public UserEntity getUserById(Long id) {
         return userRepository.
                 findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+    }
+
+    public OutgoingUserDTO getUserDetailsByJwtToken(String jwt) {
+        Long userId = jwtService.getUserIdFromToken(jwt);
+        return OutgoingUserDTO.transformUserEntity(getUserById(userId));
     }
 }
