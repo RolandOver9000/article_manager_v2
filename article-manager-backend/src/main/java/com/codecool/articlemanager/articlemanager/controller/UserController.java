2e6935bf65806c330b0948d9adf95bec9b5f8451
Roland Csosz <roland@iot-inspector.com>
2021-09-02 15:46:36 +0200

    Implemented an endpoint for updating user data and changed the getUserDetails endpoint's url

--- article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/controller/UserController.java
+++ article-manager-backend/src/main/java/com/codecool/articlemanager/articlemanager/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.codecool.articlemanager.articlemanager.controller;
 
-import com.codecool.articlemanager.articlemanager.model.dto.OutgoingUserDTO;
+import com.codecool.articlemanager.articlemanager.model.dto.UserDTO;
 import com.codecool.articlemanager.articlemanager.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +18,24 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("")
-    public ResponseEntity<OutgoingUserDTO> getUserDetails(@CookieValue(value="JWT") String jwt) {
+    @GetMapping("/logged-in")
+    public ResponseEntity<UserDTO> getUserDetails(@CookieValue(value="JWT") String jwt) {
         try{
             return ResponseEntity.ok(userService.getUserDetailsByJwtToken(jwt));
         } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "Error during getting user details.");
+        }
+    }
+
+    @PutMapping("")
+    public ResponseEntity<String> updateUserDetails(@RequestBody UserDTO userDTO) {
+        try{
+            userService.updateUserDetailsById(userDTO);
+            return ResponseEntity.ok("User successfully updated.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Error during updating user details.");
         }
     }
 }
